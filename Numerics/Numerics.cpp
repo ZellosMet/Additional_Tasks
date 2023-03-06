7bd4bcf4d6c69fa89b9cd5b68c6855006af4fd81
Zello <Zello@DESKTOP-IBMII35>
2023-03-06 11:34:58 +0900

    Numerics DONE

--- Numerics/Numerics.cpp
+++ Numerics/Numerics.cpp
@@ -5,53 +5,61 @@ using namespace std;
 Задание: В проекте Numerics реализовать перевод десятичного числа в шестнадцатеричную систему счисления (Hexadecimal).
 */
 
+#define FIRST_ASCII_NUMBER 48
+#define FIRST_ASCII_LETTER 55
+#define ASCII_LETTER_F 70
+#define HIDE -1
+
+void Show_Array(char arr[], int size);
+
 void main()
 {
 	setlocale(LC_ALL, "ru");
-	const int SIZE = 16;
-	long long dec;
+	const int NUMBER_SYSTEM = 16;
+	long long dec_number;
 
-	cout << "Введите число для преобразования в шестнадцатиричную систему "; cin >> dec;
+	cout << "Введите число для преобразования в шестнадцатиричную систему "; cin >> dec_number;
 	cout << endl;
 
-	char dex_value[SIZE] = {};
+	char dex_number[NUMBER_SYSTEM] = {};
 
-	if (dec > 0)
+	if (dec_number > 0)
 	{
-		for (int j = SIZE; j > 0; )
+		for (int j = NUMBER_SYSTEM; j > 0; )
 		{
-			while (dec)
+			while (dec_number)
 			{
-				dex_value[j - 1] = dec % 16 < 10 ? dec % 16 + 48 : dec % 16 + 55;
-				dec /= 16;
+				dex_number[j - 1] = dec_number % NUMBER_SYSTEM < 10 ? dec_number % NUMBER_SYSTEM + FIRST_ASCII_NUMBER : dec_number % NUMBER_SYSTEM + FIRST_ASCII_LETTER;
+				dec_number /= NUMBER_SYSTEM;
 				j--;
 			}
 			j--;
-			dex_value[j] = -1; //костыль
+			dex_number[j] = HIDE;
 		}
-		for (int i = 0; i < SIZE; i++)
-		if (dex_value[i] != int(-1)) cout << dex_value[i]; //работа через костыль
+		Show_Array(dex_number, NUMBER_SYSTEM);
 	}
-	else
+	else if (dec_number < 0)
 	{
-		dec = -dec-1;
-		
-		for (int j = SIZE; j > 0; )
+		dec_number = -dec_number - 1;
+		for (int j = NUMBER_SYSTEM; j > 0; )
 		{
-			while (dec)
+			while (dec_number)
 			{
-				dex_value[j - 1] = 15 - dec % 16 < 10 ? (15 - dec % 16) + 48 : (15 - dec % 16) + 55;
-				dec /= 16;
+				dex_number[j - 1] = NUMBER_SYSTEM - 1 - dec_number % NUMBER_SYSTEM < 10 ? (NUMBER_SYSTEM - 1 - dec_number % NUMBER_SYSTEM) + FIRST_ASCII_NUMBER : (NUMBER_SYSTEM - 1 - dec_number % NUMBER_SYSTEM) + FIRST_ASCII_LETTER;
+				dec_number /= NUMBER_SYSTEM;
 				j--;
 			}
-		j--;
-		dex_value[j] = -1; //костыль
-	}
-		for (int i = 0; i < SIZE; i++)
-		{
-			if (dex_value[i] == int(-1)) cout << (char)70; //работа через костыль
-			else cout << dex_value[i];
+			j--;
+			dex_number[j] = ASCII_LETTER_F;
 		}
+		Show_Array(dex_number, NUMBER_SYSTEM);
 	}
+	else cout << "Результат: " << dec_number << endl;
+}
 
+void Show_Array(char arr[], int size)
+{
+	cout << "Результат: ";
+	for (int i = 0; i < size; i++) if (arr[i] != int(HIDE)) cout << arr[i];
+	cout << endl;
 }
